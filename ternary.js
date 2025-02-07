d32ebf566756087a2541047bde976f874915c939
ridwanulbasit <ridwanul.basit32@gmail.com>
2025-02-08 02:18:53 +0600

    Ternary operator

---
+++ ternary.js
@@ -0,0 +1,17 @@
+// basic ternary operator
+
+// normally we use
+let age=18;
+
+if(age>=18){
+    console.log("Voter");
+}
+else{
+    console.log("Non Voter");
+}
+
+// using this in ternary  condition
+
+age>=18 ? console.log("Voter") : console.log("Non Voter");
+
+//
