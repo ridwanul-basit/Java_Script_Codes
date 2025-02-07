e77d2d8c5aab8dc7295c48826791b7e0c5a27a28
ridwanulbasit <ridwanul.basit32@gmail.com>
2025-02-08 01:53:11 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,38 @@
+// variable 
+var ag=23    // variable declare old version
+let id=222    // variable declare update version
+const religion="Islam"     //fixed variable can not be changed
+
+// basic 
+
+let a=5;
+let b=7;
+let sum=a+b;
+console.log(sum)
+
+let firstName="Ridwanul";
+let lastName="Basit";
+console.log(firstName+" "+lastName)
+
+// convert string to integer
+
+let c="15";
+let d=5;
+//console.log(c+d)  //it will print 155
+console.log(parseInt(c)+d)  // c will be converted into integer then it will add with d
+
+// same as we can convert float from string by using parseFloat()
+
+let e=5.666666;
+let f=5.11111;
+let total=e+f;
+console.log(total.toFixed(3))  // it will take 3 number after decimal
+
+//operator
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a**b)
+console.log(7%5)
