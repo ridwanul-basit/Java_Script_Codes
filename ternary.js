4eeb38940eb017a53ba74e42ec799103d6b14eb6
ridwanulbasit <ridwanul.basit32@gmail.com>
2025-02-11 22:43:55 +0600

    Ternary Done

--- ternary.js
+++ ternary.js
@@ -15,3 +15,26 @@ else{
 age>=18 ? console.log("Voter") : console.log("Non Voter");
 
 //
+
+// another example
+
+let price=500;
+const isLeader=false;
+
+if (isLeader===true){
+    price=0;
+}
+else{
+    price+=100;
+}
+console.log(price)
+
+// in ternary
+
+price=isLeader===true ? 0 : price+=100;
+
+console.log(price)
+
+// advance ternary
+
+price=isLeader===true ? price>1000 ? price/2 : 0 : price+=100;
