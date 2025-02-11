b48af983af8a5e2dfc11c58bcf6c2f01c90be569
ridwanulbasit <ridwanul.basit32@gmail.com>
2025-02-11 23:09:54 +0600

    Loop (for loop & while loop)

---
+++ loop.js
@@ -0,0 +1,18 @@
+// basic loop 
+
+// introduction to for loop
+
+for (let i=0; i< 10;i++) {     // ( initialization ; condition ; increment/decrement)
+
+    console.log(i);     //it will print 0 to 9
+}
+
+
+// introduction to while loop
+
+let num=1;
+
+while (num<=10){     //  condition 
+    console.log(num);    //  work in loop
+    num+=1    //  increment of loop
+}
