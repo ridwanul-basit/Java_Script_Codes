69c92e5f442141761c19c54f82a8bef99b5c22b6
ridwanulbasit <ridwanul.basit32@gmail.com>
2025-02-11 22:50:04 +0600

    Switch Case

---
+++ switch.js
@@ -0,0 +1,13 @@
+// switch statement
+
+let color="yellow";
+switch (color){
+    case "green":
+        console.log("Color matched and it's green");
+        break;
+    case "red":
+        console.log("Color matched and it's red");
+        break;
+    default:
+        console.log("Color don't matched");
+}
