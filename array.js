62826a36f7b336c9920a6c745cbdb88c6f476946
ridwanulbasit <ridwanul.basit32@gmail.com>
2025-02-11 22:57:46 +0600

    Array Basic

---
+++ array.js
@@ -0,0 +1,24 @@
+// array
+
+let number=[1,2,3,4,5];
+console.log(number);
+console.log(number.length);
+console.log(number[2]);
+number[0]=0;
+console.log(number);
+number.push(6);
+console.log(number);
+number.pop();
+console.log(number);
+number.shift();
+console.log(number);
+number.unshift(1);
+console.log(number);
+console.log(number.includes(3));
+console.log(number.includes(7));
+console.log(number.indexOf(4));
+console.log(number.slice(1,3));
+console.log(number.join("+"));
+let num2=[7,8,9]
+console.log(number.concat(num2));
+
